9801ee5bc56a1d1cf0d0cdb75c8bb9d219b3ce1b
jimish <jimishparekh123@outlook.com>
2016-07-20 18:56:48 +0530

    vitrual env setup, requirements txt added, updated gitignore

--- oizom_reports_api/settings.py
+++ oizom_reports_api/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'oizom_reports_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
