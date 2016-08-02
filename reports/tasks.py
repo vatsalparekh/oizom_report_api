0d22cfb62dbba448c981d9b43651d1f40f63e5fd
Parthvi Vala <parthvi.vala@gmail.com>
2016-08-02 23:52:16 +0530

    Pdf and HTML files delete after mailing

--- reports/tasks.py
+++ reports/tasks.py
@@ -2,7 +2,7 @@ from djcelery import celery
 from html_render import html_generate
 from pdf import pdf_generate
 from mailer import send_mail
-
+import os
 
 @celery.task
 def send_report(user_id, device_id, lte, gte, mail_id, label,
@@ -14,5 +14,6 @@ def send_report(user_id, device_id, lte, gte, mail_id, label,
             user_id, device_id, lte, gte, label, location)
         pdf_name = pdf_generate(html_name, label)
         send_mail(pdf_name, mail_id)
-
+        os.system("rm ../oizom_report_api/"+pdf_name)
+        os.system("rm ../oizom_report_api/"+html_name)
     print 'Done!' + label + mail_id
