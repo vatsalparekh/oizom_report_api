a5bcd44da0ba2145c5a08901ef6e3a8491c11417
Vatsal Parekh <vatsalparekh@outlook.com>
2016-11-14 17:12:48 +0530

    Added setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo apt-get -y upgrade;
 sudo apt-get -y install htop wkhtmltopdf python-pip rabbitmq-server git;
 cd modules/HTML.py-0.04/;
 python setup.py install;
-cd ..; cd..;
+cd ..; cd ..;
 pip install -r requirements.txt;
