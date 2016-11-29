d6354257c7afaad82887a178bf6ed5dc63b79007
Vatsal Parekh <vatsalparekh@outlook.com>
2016-11-29 22:55:58 +0530

    Removed replacement of source binary installation

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ cd modules/HTML.py-0.04/
 sudo python setup.py install
 cd ..; cd ..;
 sudo pip install -r requirements.txt
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-tar xf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-sudo cp wkhtmltox/bin/wkhtmltopdf /usr/bin/
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xf phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin
