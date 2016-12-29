098a2bad1954d38e877974fe25194d38747f2474
Vatsal Parekh <vatsalparekh@outlook.com>
2016-12-29 19:26:35 +0530

    Changes in [] with {}

--- csv_app/views.py
+++ csv_app/views.py
@@ -14,120 +14,240 @@ def csv_update(request):
     data = json.loads(str(request.body).decode('utf-8'))
 
     try:
-        for items in data:
+        if type(data) == list:
+
+            for items in data:
+                doc = document()
+
+                try:
+                    doc.deviceId = items['deviceId']
+                except Exception:
+                    pass
+
+                try:
+                    doc.label = items['label']
+                except Exception:
+                    pass
+
+                try:
+                    doc.loc = items['loc']
+                except Exception:
+                    pass
+
+                try:
+                    doc.latitude = items['lat']
+                except Exception:
+                    pass
+
+                try:
+                    doc.longitude = items['long']
+                except Exception:
+                    pass
+
+                try:
+                    doc.t = datetime.fromtimestamp(int(items['t']))
+                    print (doc.t)
+                except Exception as e:
+                    print (e)
+
+                try:
+                    doc.AQI = items['aqi']
+                except Exception:
+                    pass
+
+                try:
+                    doc.PM10 = items['pm10']
+                except KeyError:
+                    try:
+                        doc.PM10 = items['p2']
+                    except Exception:
+                        pass
+
+                try:
+                    doc.PM25 = items['pm25']
+                except KeyError:
+                    try:
+                        doc.PM25 = items['p1']
+                    except Exception:
+                        pass
+
+                try:
+                    doc.CO = items['g2']
+                except KeyError:
+                    try:
+                        doc.CO = items['co']
+                    except KeyError:
+                        pass
+
+                try:
+                    doc.O3 = items['g5']
+                except KeyError:
+                    try:
+                        doc.O3 = items['o3']
+                    except Exception:
+                        pass
+
+                try:
+                    doc.SO2 = items['g8']
+                except KeyError:
+                    try:
+                        doc.SO2 = items['so2']
+                    except Exception:
+                        pass
+
+                try:
+                    doc.NO2 = items['g7']
+                except KeyError:
+                    try:
+                        doc.NO2 = items['no2']
+                    except KeyError:
+                        pass
+
+                try:
+                    doc.temp = items['temp']
+                except Exception:
+                    pass
+
+                try:
+                    doc.hum = items['hum']
+                except Exception:
+                    pass
+
+                try:
+                    doc.WD = items['wd']
+                except Exception:
+                    pass
+
+                try:
+                    doc.WS = items['ws']
+                except Exception:
+                    pass
+
+                try:
+                    doc.pressure = items['prs']
+                except Exception:
+                    pass
+
+                try:
+                    doc.rainGaige = items['rg']
+                except Exception:
+                    pass
+
+                doc.save()
+        elif type(data) == dict:
             doc = document()
 
             try:
-                doc.deviceId = items['deviceId']
+                doc.deviceId = data['deviceId']
             except Exception:
                 pass
 
             try:
-                doc.label = items['label']
+                doc.label = data['label']
             except Exception:
                 pass
 
             try:
-                doc.loc = items['loc']
+                doc.loc = data['loc']
             except Exception:
                 pass
 
             try:
-                doc.latitude = items['lat']
+                doc.latitude = data['lat']
             except Exception:
                 pass
 
             try:
-                doc.longitude = items['long']
+                doc.longitude = data['long']
             except Exception:
                 pass
 
             try:
-                doc.t = datetime.fromtimestamp(int(items['t']))
+                doc.t = datetime.fromtimestamp(int(data['t']))
                 print (doc.t)
             except Exception as e:
                 print (e)
 
             try:
-                doc.AQI = items['aqi']
+                doc.AQI = data['aqi']
             except Exception:
                 pass
 
             try:
-                doc.PM10 = items['pm10']
+                doc.PM10 = data['pm10']
             except KeyError:
                 try:
-                    doc.PM10 = items['p2']
+                    doc.PM10 = data['p2']
                 except Exception:
                     pass
 
             try:
-                doc.PM25 = items['pm25']
+                doc.PM25 = data['pm25']
             except KeyError:
                 try:
-                    doc.PM25 = items['p1']
+                    doc.PM25 = data['p1']
                 except Exception:
                     pass
 
             try:
-                doc.CO = items['g2']
+                doc.CO = data['g2']
             except KeyError:
                 try:
-                    doc.CO = items['co']
+                    doc.CO = data['co']
                 except KeyError:
                     pass
 
             try:
-                doc.O3 = items['g5']
+                doc.O3 = data['g5']
             except KeyError:
                 try:
-                    doc.O3 = items['o3']
+                    doc.O3 = data['o3']
                 except Exception:
                     pass
 
             try:
-                doc.SO2 = items['g8']
+                doc.SO2 = data['g8']
             except KeyError:
                 try:
-                    doc.SO2 = items['so2']
+                    doc.SO2 = data['so2']
                 except Exception:
                     pass
 
             try:
-                doc.NO2 = items['g7']
+                doc.NO2 = data['g7']
             except KeyError:
                 try:
-                    doc.NO2 = items['no2']
+                    doc.NO2 = data['no2']
                 except KeyError:
                     pass
 
             try:
-                doc.temp = items['temp']
+                doc.temp = data['temp']
             except Exception:
                 pass
 
             try:
-                doc.hum = items['hum']
+                doc.hum = data['hum']
             except Exception:
                 pass
 
             try:
-                doc.WD = items['wd']
+                doc.WD = data['wd']
             except Exception:
                 pass
 
             try:
-                doc.WS = items['ws']
+                doc.WS = data['ws']
             except Exception:
                 pass
 
             try:
-                doc.pressure = items['prs']
+                doc.pressure = data['prs']
             except Exception:
                 pass
 
             try:
-                doc.rainGaige = items['rg']
+                doc.rainGaige = data['rg']
             except Exception:
                 pass
 
