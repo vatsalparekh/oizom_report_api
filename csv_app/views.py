e406e9d349b08e047817e516364ab6204e1378c3
Vatsal Parekh <vatsalparekh@outlook.com>
2017-01-04 16:46:55 +0530

    Update views.py

--- csv_app/views.py
+++ csv_app/views.py
@@ -273,7 +273,7 @@ def retrive(request, deviceid, gte, lte):
     except Exception as e:
         print e
 
-    return render_to_csv_response(doc)
+    return render_to_csv_response(doc, filename=str(deviceid), append_datestamp=True)
 
 
 @api_view(['GET'])
